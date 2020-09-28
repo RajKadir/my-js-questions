5465a647deb06f424d6ed09c8f8f01689f08a946
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-28 16:58:29 +0100

    chore: add question 7: use Object.prototype.toString.call() method to check if array is an Array and not an Object

--- questions/q7.js
+++ questions/q7.js
@@ -1,3 +1,19 @@
 /*
     Question 7. How to check if an object is an array or not?
 */
+
+/*
+    Answer: use the toString method to check class type on Object.prototype
+*/
+var arrayList = ["a", "b", "c"];
+console.log(Object.prototype.toString.call(arrayList)); // output: [object Array]
+
+if (Object.prototype.toString.call(arrayList) === "[object Array]") {
+  console.log("This is an array");
+}
+
+// Proof on objects
+var objectVar = {
+  x: "hello",
+};
+console.log(Object.prototype.toString.call(objectVar)); // output: [object Object]
