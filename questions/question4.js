b913f96cd6cf8d0e03bed9bd5173d5f6af84d2b8
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-28 16:16:51 +0100

    chore: add q4, q5 templates

---
+++ questions/question4.js
@@ -0,0 +1,8 @@
+/*
+    Question 4. What is “closure” in javascript? Can you provide an example?
+*/
+
+/*
+    Answer
+    
+*/
---
+++ questions/question5.js
@@ -0,0 +1,6 @@
+/*
+    Question 5. Write a mul function which will work properly when invoked with following syntax.
+
+    console.log(mul(2)(3)(4)); // output : 24
+    console.log(mul(4)(3)(4)); // output : 48
+*/
