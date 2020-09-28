5d7c6db288bce334144a6aa5d4f58ff59db0e8d9
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-28 17:00:09 +0100

    chore: add answer 2 and 3 to question 7

--- questions/q7.js
+++ questions/q7.js
@@ -17,3 +17,12 @@ var objectVar = {
   x: "hello",
 };
 console.log(Object.prototype.toString.call(objectVar)); // output: [object Object]
+
+/*
+    Answer 2: You can use isArray if you are using jQuery i.e $.isArray(arrayList)
+*/
+
+/*
+    Answer 3: In mordern browsers you can use Array.isArray(arrayList)
+    IMPORTANT: Array.isArray is supported by Chrome 5, Firefox 4.0, IE 9, Opera 10.5 and Safari 5
+*/
