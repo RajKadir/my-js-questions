21ae3c4c46f966991f937f409cab473389dde891
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-28 16:28:00 +0100

    chore: added question 4

--- questions/question4.js
+++ questions/question4.js
@@ -4,5 +4,23 @@
 
 /*
     Answer
-    
+    Closure is a function inside a function (called parent function) and as such it has access to the variables declared and defined within its parent function's scope.
 */
+
+/* 
+    Example
+*/
+
+var globalVar = "Raj";
+
+// Self invoking functions
+
+(function parentFunc(parentArgs) {
+  console.log(globalVar); // output Raj
+
+  (function childFunc(childArgs) {
+    console.log(parentArgs); // output 10
+    console.log(childArgs); // output 5
+    console.log(globalVar); // output Raj
+  })(5);
+})(10);
