b53939d560f14280ea9e593763ce7ee4c3d9cad2
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-28 15:58:40 +0100

    chore: add NaN answer to question 2

--- questions/question2.js
+++ questions/question2.js
@@ -2,3 +2,14 @@
     if( x <= 100 ) {...}
     if( !(x > 100) ) {...}
 */
+
+/* Answer: 
+    NaN <= 100 is false
+    NaN > 100 is false
+    so if x is NaN the statements are not the same
+
+    This holds true for any value of x being converted to type Number, returns NaN, e.g. undefined, [1,2,3], {a:20}
+
+    Pay attention: NaN can't be equal, less than or more than any other numerical value, so the only reliable way to check if the value is NaN, is to use the isNaN() function
+
+*/
