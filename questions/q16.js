7b255252be1eb3cb0c33cbf2ebdb804316214cb4
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-29 14:46:15 +0100

    chore: finished question 16

--- questions/q16.js
+++ questions/q16.js
@@ -1,6 +1,18 @@
 /*
-    Question 16. What will be the output of the following code?
-    // NFE (Named Function Expression)
-    var foo = function bar() { return 12; };
-    typeof bar();
+  Question 16. What will be the output of the following code?
+  // NFE (Named Function Expression)
+  var foo = function bar() { return 12; };
+  typeof bar();
 */
+
+/*
+  Answer: ReferenceError, because bar is not a reference variable it is a name of a function, when using typeof we use it on variables that need to be references in this cause type foo() would work.
+*/
+
+/*
+  Code fixed so that typeof would work
+*/
+var bar = function () {
+  return 12;
+};
+typeof bar();
