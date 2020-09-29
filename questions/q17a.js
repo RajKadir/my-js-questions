0b8c40bd05c3a6992af0d7ba3e2f282a1b0de3b4
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-29 14:54:04 +0100

    chore: finished q17a

--- questions/q17a.js
+++ questions/q17a.js
@@ -9,3 +9,25 @@
         // Some code
     }
 */
+
+/*
+    Answer:
+        The difference is that the function 'foo' is declared at 'run-time' and is called a 'function expression'. 
+        Where as the function 'bar' is defined at 'parse time' and is called a 'function statement'.
+*/
+
+/* 
+    Example of showing the difference between run-time and parse time
+*/
+
+// Parse-Time function declaration
+bar(); // Call bar function here, It will not give an Error because we created bar at parse-time and so we can use it anywhere
+function bar() {
+  console.log("Hi I am inside Foo");
+}
+
+// Run-Time function declaration
+foo(); // Call foo function here, It will give an error because we created foo at run-time, and at this current moment, the function foo() doesn't exist
+var foo = function () {
+  console.log("Hi I am inside Foo");
+};
