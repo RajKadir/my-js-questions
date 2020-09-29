b1456dda0ee774d70dbbeb3ae2c0e761a593b7fc
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-29 14:49:27 +0100

    chore: add q17a template

---
+++ questions/q17a.js
@@ -0,0 +1,11 @@
+/*
+    Question 17a. What is the difference between declaring a function in the formats listed below?
+
+    var foo = function() {
+        // Some code
+    }
+
+    function bar () {
+        // Some code
+    }
+*/
