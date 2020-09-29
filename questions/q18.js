05a4b8e6211dd05a07675b1655416d945ff5ce78
ec16449 <r.kadir@se16.qmul.ac.uk>
2020-09-29 15:03:49 +0100

    chore: added q18

--- questions/q18.js
+++ questions/q18.js
@@ -4,4 +4,32 @@
 
 /*
     Answer:
+
+        Note the function expression:
+            var foo = function foo() {
+                return 12;
+            }
+
+        In JavaScript var variables and functions are hoisted. Basically the JavaScript interpreter looks ahead to find all the variable declarations and hoists them to the top of the function where it's declared:
+        foo()
+        var foo = function foo(){
+            return 12;
+        }
+
+        Behind the scenes it will look like this
+        var foo = undefined;
+        foo(); // here foo is undefined
+        foo = function foo(){
+            return 12;
+        }
+
+        then:
+        var foo = undefined;
+        foo = function foo(){
+            return 12;
+        }
+        foo(); // Now foo is defined here
+
+
+        In summary: Before all the code is ran, JS first gets all declarations, hoisting is the name of this process.
 */
